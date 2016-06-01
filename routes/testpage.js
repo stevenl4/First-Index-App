d7c9cc3458416460c613db27bb8dba397fc06c34
stevenl4 <steven.y.luo@gmail.com>
2016-06-01 18:35:25 -0400

    Add new modules, tweak test page

--- routes/testpage.js
+++ routes/testpage.js
@@ -4,12 +4,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var data = {
+    'name': 'Steven',
+    'age': 25,
+    'gender': 'M'
+}
+console.log(data);
+/* GET testpage page. */
 router.get('/', function(req, res, next) {
     res.render('testpage', { 
         title: 'Test Page',
-        somedata: 'data'
+        somedata: data
     });
 });
 
+router.get('/nextpage', function(req, res, next) {
+    res.render('testpage', {
+        title: 'Next Page',
+        somedata: data
+    });
+});
 module.exports = router;
