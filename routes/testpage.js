71aa7676240852573f21de225c1aeca4c13ce560
stevenl4 <steven.y.luo@gmail.com>
2016-06-05 23:50:14 -0400

    read and write to local mongo db

--- routes/testpage.js
+++ routes/testpage.js
@@ -1,6 +1,8 @@
 /**
  * Created by Steven on 5/30/2016.
  */
+var mongo = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/todoItemsdb';
 var express = require('express');
 var bodyParser = require('body-parser');
 var router = express.Router();
@@ -11,11 +13,31 @@ var data = {
     'gender': 'M'
 }
 
-var todoItems = [
-    { id: 1, desc: 'foo' },
-    { id: 2, desc: 'bar' },
-    { id: 3, desc: 'baz' }
-]
+var todoItems = [];
+//     { id: 1, desc: 'foo' },
+//     { id: 2, desc: 'bar' },
+//     { id: 3, desc: 'baz' }
+// ]
+
+mongo.connect(url, function(err, db){
+    if (err) throw err
+    var collection = db.collection('item');
+    collection.find({
+        id: {
+            $gt: 0
+        }
+    }, {
+        id: 1,
+        desc: 1,
+        _id: 0
+    }).toArray(function (err, docs){
+        if (err) throw err;
+        docs.forEach(function(item,index){
+            todoItems.push(item);
+        })
+        db.close();
+    })
+})
 
 /* GET testpage page. */
 router.get('/', function(req, res, next) {
@@ -35,11 +57,19 @@ router.get('/', function(req, res, next) {
 
 router.post('/add', function(req, res, next) {
     var newItem = req.body.newItem;
-
-    todoItems.push( {
+    var newObj = {
         id: todoItems.length + 1,
         desc: newItem
-    });
+    };
+    mongo.connect(url, function(err, db){
+        if (err) throw err
+        var collection = db.collection('item');
+        collection.insertOne(newObj, function(err, docs){
+            if (err) throw err;
+            db.close();
+        })
+    })
+    todoItems.push(newObj);
     
     res.redirect('/testpage');
 });
