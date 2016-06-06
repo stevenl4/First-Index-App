8bef2f780340320b945f0c33a57ff4d5649aa815
stevenl4 <steven.y.luo@gmail.com>
2016-06-05 23:14:17 -0400

    add post to test page

--- routes/testpage.js
+++ routes/testpage.js
@@ -2,6 +2,7 @@
  * Created by Steven on 5/30/2016.
  */
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 var data = {
@@ -9,19 +10,37 @@ var data = {
     'age': 25,
     'gender': 'M'
 }
-console.log(data);
+
+var todoItems = [
+    { id: 1, desc: 'foo' },
+    { id: 2, desc: 'bar' },
+    { id: 3, desc: 'baz' }
+]
+
 /* GET testpage page. */
 router.get('/', function(req, res, next) {
     res.render('testpage', { 
         title: 'Test Page',
-        somedata: data
+        somedata: data,
+        todoItems: todoItems
     });
 });
 
-router.get('/nextpage', function(req, res, next) {
-    res.render('testpage', {
-        title: 'Next Page',
-        somedata: data
+// router.get('/add', function(req, res, next) {
+//     res.render('testpage', {
+//         title: 'Next Page',
+//         somedata: data
+//     });
+// });
+
+router.post('/add', function(req, res, next) {
+    var newItem = req.body.newItem;
+
+    todoItems.push( {
+        id: todoItems.length + 1,
+        desc: newItem
     });
+    
+    res.redirect('/testpage');
 });
 module.exports = router;
